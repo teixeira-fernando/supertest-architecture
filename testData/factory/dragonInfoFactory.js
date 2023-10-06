db0bd53bf3e38083048e52667a4b80822798863a
teixeira-fernando <fernandoadt1@gmail.com>
2023-10-06 14:22:28 +0200

    refactoring to test the new application under test

---
+++ testData/factory/dragonInfoFactory.js
@@ -0,0 +1,15 @@
+import allDragonsInfoQuery from '../queries/allDragonsInfoQuery';
+import singleDragoninfoQuery from '../queries/singleDragoninfoQuery';
+
+class DragonInfoFactory {
+  getInfoAllDragons() {
+    return allDragonsInfoQuery;
+  }
+
+  getInfoSingleDragon(id){
+    singleDragoninfoQuery.variables.dragonId = id 
+    return singleDragoninfoQuery
+  }
+}
+
+export default new DragonInfoFactory();
